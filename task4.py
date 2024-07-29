3f19c517e026a13fc4eadbb8e0e15127ca82cb5b
mynameisdasha <128239152+mynameisdasha@users.noreply.github.com>
2024-07-29 05:28:59 +0300

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,14 @@
+import math
+def min_moves(nums):
+    average = sum(nums) // len(nums)
+    moves = 0
+    for num in nums:
+        moves += abs(num - average)
+    return moves
+with open("posch.txt", "r", encoding='utf-8') as p:
+    o=p.read()
+wo=str(o)
+normal_string = wo.replace('\n', ' ')
+numb=normal_string.split()
+int_numb=[int(num) for num in numb]
+print(min_moves(int_numb))
