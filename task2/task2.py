ca24ccfd0b26d7c24aabbb947c4baa9780b83916
mynameisdasha <128239152+mynameisdasha@users.noreply.github.com>
2024-07-29 06:37:34 +0300

    Add files via upload

--- task2/task2.py
+++ task2/task2.py
@@ -1,17 +1,25 @@
 import math
 with open("file1.txt", "r", encoding='utf-8') as f:
     o=f.read()
-centr=(int(o[0]),int(o[2]))
+wo=str(o)
+normal_string = wo.replace('\n', ' ')
+numb=normal_string.split()
+int_numb=[int(num) for num in numb]
+centr=(int_numb[0],int_numb[1])
 print(centr)
-radius=int(o[4])
+radius=int_numb[2]
 print(radius)
 with open("file2.txt", "r", encoding='utf-8') as f:
     o1=f.read()
-point1=(int(o1[0]),int(o1[2]))
+wo1=str(o1)
+normal_string1 = wo1.replace('\n', ' ')
+numb1=normal_string1.split()
+int_numb1=[int(num) for num in numb1]
+point1=(int_numb1[0],int_numb1[1])
 print(point1)
-point2=(int(o1[4]),int(o1[6]))
+point2=(int_numb1[2],int_numb1[3])
 print(point2)
-point3=(int(o1[8]),int(o1[10]))
+point3=(int_numb1[4],int_numb1[5])
 print(point3)
 def func(centr,radius,point):
     if (math.dist(point, centr) < radius):
